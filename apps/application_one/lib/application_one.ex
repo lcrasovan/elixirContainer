66105ade56f61e545f46b4c66adb70872e818873
lucian crasovan <l.crasovan@resto-in.com>
2016-07-26 15:25:11 +0200

    lists transformator

--- apps/application_one/lib/application_one.ex
+++ apps/application_one/lib/application_one.ex
@@ -4,6 +4,16 @@ defmodule ApplicationOne do
   @message_one Application.get_env(:application_one, :message_one)
   @message_two Application.get_env(:application_one, :message_two)
 
+  @default_list [1,2,3,4,5,6]
+
+  def get_odd_numbers(list \\ @default_list) do
+    Enum.filter(list, fn(x)-> rem(x,2) == 1 end)
+  end
+
+  def get_even_numbers(list \\ @default_list) do
+    Enum.filter(list, fn(x)-> rem(x,2) == 0 end)
+  end
+
   def show_messages do
     IO.puts "Message one is: #{@message_one}"
     IO.puts "Message two is: #{@message_two}"
