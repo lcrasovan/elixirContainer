3146906113cf2c6a9b37707fb52e55844ee54f01
lucian crasovan <l.crasovan@resto-in.com>
2016-11-20 00:37:31 +0100

    improving testing - read fixture from file line by line

--- apps/shakespeareBrowser/test/shakespeareBrowser_test.exs
+++ apps/shakespeareBrowser/test/shakespeareBrowser_test.exs
@@ -7,9 +7,32 @@ defmodule ShakespeareBrowserTest do
   end
 
   test "counting words in phrases" do
-    #cummulating phrases - super text enters super test
+    #cumulating phrases - super text enters super test
     text = ["hey jude", "hey jude hey"]
     assert WordCount.text_count(text) == %{hey: 3, jude: 2}
+  end
+
+  test "reading from file line by line into array" do
+
+    text = []
+    filename = "test/fixtures/hamlet.txt"
+    if File.exists?(filename) do
+      stream = File.stream!(filename, [:read, :utf8])
+      text = Enum.reduce stream, text, fn(line, listing) ->
+               [process_line(line)] ++ [listing]
+             end
+      end
+    assert WordCount.text_count(text) == %{FRANCISCO: 1, BERNARDO: 1, Enter: 1, Nay: 1, "Who's": 1, and: 1, answer: 1, at: 1, him: 1, his: 1, me: 1, post: 1, stand: 1, there: 1, to: 1, unfold: 1, yourself: 1}
+  end
+
+  def process_line(line) do
+    line
+      |> String.strip
+      |> String.replace(".", " ")
+      |> String.replace(",", " ")
+      |> String.replace(":", " ")
+      |> String.replace(";", " ")
+      |> String.replace("?", " ")
   end
 
 end
